f0bd43cc44b9fe458c72ed0fc42b73a4f6519cec
babarpriya <priyababar455@gmail.com>
2025-05-23 10:57:54 +0530

    Create checknumber.js

---
+++ checknumber.js
@@ -0,0 +1,11 @@
+function Number(num) {
+    if (num > 0) {
+        console.log(" is positive");
+    } else if (num < 0) {
+        console.log(" is negative");
+    } else {
+        console.log("The number is zero");
+    }
+}
+
+Number(0); 
